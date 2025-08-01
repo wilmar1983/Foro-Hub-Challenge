ca42a755c3fe7c40bc4872c78335a7e1284cbfc2
wilmar1983 <wrgkoky@gmail.com>
2025-07-31 21:04:08 -0500

    Initial commit with project setup

---
+++ src/main/java/com/aluracursos/foro_hub_challenge/ForoHubChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.aluracursos.foro_hub_challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubChallengeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aluracursos/foro_hub_challenge/ForoHubChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aluracursos.foro_hub_challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ForoHubChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
